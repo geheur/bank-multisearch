501d32053457890fac6449ac0dde618ef61afae1
sam <dasistkeinnamen@gmail.com>
2023-07-18 05:09:45 +0100

    update

--- src/main/java/com/bankmultisearch/BankMultisearchConfig.java
+++ src/main/java/com/bankmultisearch/BankMultisearchConfig.java
@@ -8,12 +8,32 @@ import net.runelite.client.config.ConfigItem;
 public interface BankMultisearchConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = "multiSearchEnabled",
+		name = "Multisearch",
+		description = "Search for all words in the search separately, e.g. search \"chisel sapphire\" to show all items that contain EITHER \"chisel\" or \"sapphire\".",
+		position = 10
 	)
-	default String greeting()
-	{
-		return "Hello";
+	default boolean multiSearchEnabled() {
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "slotSearchEnabled",
+		name = "Slot search",
+		description = "Including a slot name in the search to only show items that fit in that slot. You can include multiple slots. Remember that new items may not show up because they have not been picked up by the runelite wiki scraper.",
+		position = 20
+	)
+	default boolean slotSearchEnabled() {
+		return true;
+	}
+
+	@ConfigItem(
+		keyName = "bla",
+		name = "<html>Available slot search terms: leg, legs, chest, body, torso, glove, gloves, hand, hands, head, helm, hat, boot, boots, foot, feet, amulet, necklace, neck, ring, shield, offhand, cape, cloak, back, ammo, weapon, 1h, mainhand, 1hweapon, weaponslot1h, 2h, 2hweapon, weaponslot2h.</html>",
+		description = "",
+		position = 30
+	)
+	default void label1() {
+
 	}
 }
--- src/main/java/com/bankmultisearch/BankMultisearchPlugin.java
+++ src/main/java/com/bankmultisearch/BankMultisearchPlugin.java
@@ -3,13 +3,16 @@ package com.bankmultisearch;
 import com.google.gson.Gson;
 import com.google.inject.Provides;
 import java.util.*;
+import java.util.function.Consumer;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.EquipmentInventorySlot;
 import net.runelite.api.events.*;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.EventBus;
+import net.runelite.client.eventbus.EventBus.Subscriber;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.game.ItemManager;
 import net.runelite.client.input.KeyManager;
@@ -17,10 +20,12 @@ import net.runelite.client.input.MouseManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.http.api.item.ItemStats;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Bank Multisearch"
+	name = "Bank search utilities",
+	tags = {"slot"}
 )
 public class BankMultisearchPlugin extends Plugin
 {
@@ -38,11 +43,13 @@ public class BankMultisearchPlugin extends Plugin
 	@Override
 	protected void startUp()
 	{
+		updateSubscribers();
 	}
 
 	@Override
 	protected void shutDown()
 	{
+		removeSubscribers();
 	}
 
 	@Provides
@@ -51,255 +58,84 @@ public class BankMultisearchPlugin extends Plugin
 		return configManager.getConfig(BankMultisearchConfig.class);
 	}
 
+	private final List<Subscriber> subscribers = new ArrayList<>();
+
+	@Subscribe
+	public void onGameTick(GameTick e) {
+		updateSubscribers();
+	}
+
+	private void updateSubscribers() {
+		removeSubscribers();
+
+		register(GameTick.class, e -> {
+//			System.out.println("in subscriber");
+		});
+	}
+
+	private <T> void register(Class<T> event, Consumer<T> eventHandler) {
+		register(event, eventHandler, 1f);
+	}
+
+	private <T> void register(Class<T> event, Consumer<T> eventHandler, float priority) {
+		Subscriber subscriber = eventBus.register(event, eventHandler, priority);
+		subscribers.add(subscriber);
+	}
+
+	private void removeSubscribers() {
+		for (Subscriber subscriber : subscribers)
+		{
+			eventBus.unregister(subscriber);
+		}
+		subscribers.clear();
+	}
+
 	private final Map<String, Object> vars = new HashMap<>();
-
-	@Subscribe
-	public void onAccountHashChanged(AccountHashChanged e) {
-	}
-
-	@Subscribe
-	public void onActorDeath(ActorDeath e) {
-	}
-
-	@Subscribe
-	public void onAnimationChanged(AnimationChanged e) {
-	}
-
-	@Subscribe
-	public void onAreaSoundEffectPlayed(AreaSoundEffectPlayed e) {
-	}
-
-	@Subscribe
-	public void onBeforeMenuRender(BeforeMenuRender e) {
-	}
-
-	@Subscribe
-	public void onBeforeRender(BeforeRender e) {
-	}
-
-	@Subscribe
-	public void onCanvasSizeChanged(CanvasSizeChanged e) {
-	}
-
-	@Subscribe
-	public void onChatMessage(ChatMessage e) {
-	}
-
-	@Subscribe
-	public void onClanChannelChanged(ClanChannelChanged e) {
-	}
-
-	@Subscribe
-	public void onClanMemberJoined(ClanMemberJoined e) {
-	}
-
-	@Subscribe
-	public void onClanMemberLeft(ClanMemberLeft e) {
-	}
-
-	@Subscribe
-	public void onClientTick(ClientTick e) {
-	}
 
 	@Subscribe
 	public void onCommandExecuted(CommandExecuted e) {
 		if ("update".equals(e.getCommand())) {
+
 		}
 	}
 
-	@Subscribe
-	public void onDecorativeObjectDespawned(DecorativeObjectDespawned e) {
-	}
-
-	@Subscribe
-	public void onDecorativeObjectSpawned(DecorativeObjectSpawned e) {
-	}
-
-	@Subscribe
-	public void onDraggingWidgetChanged(DraggingWidgetChanged e) {
-	}
-
-	@Subscribe
-	public void onFakeXpDrop(FakeXpDrop e) {
-	}
-
-	@Subscribe
-	public void onFocusChanged(FocusChanged e) {
-	}
-
-	@Subscribe
-	public void onFriendsChatChanged(FriendsChatChanged e) {
-	}
-
-	@Subscribe
-	public void onFriendsChatMemberJoined(FriendsChatMemberJoined e) {
-	}
-
-	@Subscribe
-	public void onFriendsChatMemberLeft(FriendsChatMemberLeft e) {
-	}
-
-	@Subscribe
-	public void onGameObjectDespawned(GameObjectDespawned e) {
-	}
-
-	@Subscribe
-	public void onGameObjectSpawned(GameObjectSpawned e) {
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged e) {
-	}
-
-	@Subscribe
-	public void onGameTick(GameTick e) {
-	}
-
-	@Subscribe
-	public void onGrandExchangeOfferChanged(GrandExchangeOfferChanged e) {
-	}
-
-	@Subscribe
-	public void onGrandExchangeSearched(GrandExchangeSearched e) {
-	}
-
-	@Subscribe
-	public void onGraphicChanged(GraphicChanged e) {
-	}
-
-	@Subscribe
-	public void onGraphicsObjectCreated(GraphicsObjectCreated e) {
-	}
-
-	@Subscribe
-	public void onGroundObjectDespawned(GroundObjectDespawned e) {
-	}
-
-	@Subscribe
-	public void onGroundObjectSpawned(GroundObjectSpawned e) {
-	}
-
-	@Subscribe
-	public void onHitsplatApplied(HitsplatApplied e) {
-	}
-
-	@Subscribe
-	public void onInteractingChanged(InteractingChanged e) {
-	}
-
-	@Subscribe
-	public void onItemContainerChanged(ItemContainerChanged e) {
-	}
-
-	@Subscribe
-	public void onItemDespawned(ItemDespawned e) {
-	}
-
-	@Subscribe
-	public void onItemQuantityChanged(ItemQuantityChanged e) {
-	}
-
-	@Subscribe
-	public void onItemSpawned(ItemSpawned e) {
-	}
-
-	@Subscribe
-	public void onMenuEntryAdded(MenuEntryAdded e) {
-	}
-
-	@Subscribe
-	public void onMenuOpened(MenuOpened e) {
-	}
-
-	@Subscribe
-	public void onMenuOptionClicked(MenuOptionClicked e) {
-	}
-
-	@Subscribe
-	public void onMenuShouldLeftClick(MenuShouldLeftClick e) {
-	}
-
-	@Subscribe
-	public void onNameableNameChanged(NameableNameChanged e) {
-	}
-
-	@Subscribe
-	public void onNpcChanged(NpcChanged e) {
-	}
-
-	@Subscribe
-	public void onNpcDespawned(NpcDespawned e) {
-	}
-
-	@Subscribe
-	public void onNpcSpawned(NpcSpawned e) {
-	}
-
-	@Subscribe
-	public void onOverheadTextChanged(OverheadTextChanged e) {
-	}
-
-	@Subscribe
-	public void onPlayerChanged(PlayerChanged e) {
-	}
-
-	@Subscribe
-	public void onPlayerDespawned(PlayerDespawned e) {
-	}
-
-	@Subscribe
-	public void onPlayerMenuOptionsChanged(PlayerMenuOptionsChanged e) {
-	}
-
-	@Subscribe
-	public void onPlayerSpawned(PlayerSpawned e) {
-	}
-
-	@Subscribe
-	public void onPostHealthBar(PostHealthBar e) {
-	}
-
-	@Subscribe
-	public void onPostItemComposition(PostItemComposition e) {
-	}
-
-	@Subscribe
-	public void onPostObjectComposition(PostObjectComposition e) {
-	}
-
-	@Subscribe
-	public void onPostStructComposition(PostStructComposition e) {
-	}
-
-	@Subscribe
-	public void onProjectileMoved(ProjectileMoved e) {
-	}
-
-	@Subscribe
-	public void onRemovedFriend(RemovedFriend e) {
-	}
-
-	@Subscribe
-	public void onResizeableChanged(ResizeableChanged e) {
-	}
-
-	@Subscribe
+	@Subscribe(priority = -1)
 	public void onScriptCallbackEvent(ScriptCallbackEvent e) {
+		if (!"bankSearchFilter".equals(e.getEventName())) return;
+
 		int[] intStack = client.getIntStack();
 		String[] stringStack = client.getStringStack();
 		int intStackSize = client.getIntStackSize();
 		int stringStackSize = client.getStringStackSize();
 
-		if ("bankSearchFilter".equals(e.getEventName()))
-		{
-			int itemId = intStack[intStackSize - 1];
-			String search = stringStack[stringStackSize - 1];
+		String search = stringStack[stringStackSize - 1];
+		updateSearch(search);
 
-			// TODO check if value search is happening. Same for regex search maybe?
-			String[] split = search.toLowerCase().split("\\s+");
+		int itemId = intStack[intStackSize - 1];
+		if (slotsBitfield != 0) { // any slot filters exist.
+			ItemStats itemStats = itemManager.getItemStats(itemId, false);
+			boolean wrongSlot =
+				itemStats == null ||
+				!itemStats.isEquipable() ||
+				(slotsBitfield & 1 << itemStats.getEquipment().getSlot()) == 0 ||
+				(
+					itemStats.getEquipment().getSlot() == EquipmentInventorySlot.WEAPON.getSlotIdx() &&
+					twohand != null &&
+					itemStats.getEquipment().isTwoHanded() != twohand
+				)
+			;
+			intStack[intStackSize - 2] = wrongSlot ? 0 : 1;
+			if (wrongSlot) return; // Filtered out items should not be included in the later search.
+		}
+
+		if (split != null)
+		{
+			intStack[intStackSize - 2] = 0;
+			String itemName = itemManager.getItemComposition(itemId).getName().toLowerCase();
 			for (String s : split)
 			{
-				if (itemManager.getItemComposition(itemId).getName().toLowerCase().contains(s)) {
+				if (itemName.contains(s))
+				{
 					// return true
 					intStack[intStackSize - 2] = 1;
 					break;
@@ -308,63 +144,123 @@ public class BankMultisearchPlugin extends Plugin
 		}
 	}
 
-	@Subscribe
-	public void onScriptPostFired(ScriptPostFired e) {
-	}
+	private String lastSearch = "";
+	private List<String> split = Collections.emptyList();
+	private int slotsBitfield = 0;
+	private Boolean twohand = null;
 
-	@Subscribe
-	public void onScriptPreFired(ScriptPreFired e) {
-	}
+	private void updateSearch(String search) {
+		// use old values if possible.
+		if (search.equals(lastSearch)) return;
+		lastSearch = search;
 
-	@Subscribe
-	public void onSoundEffectPlayed(SoundEffectPlayed e) {
-	}
+		split = null;
+		slotsBitfield = 0;
+		twohand = null;
 
-	@Subscribe
-	public void onStatChanged(StatChanged e) {
-	}
+		// detect bank value searching, we don't want to search for "ge" or "ha" accidentally. <>= are not used in any item names so this should not interfere with people's use of the plugin.
+		if (search.matches(".*[<>=].*")) {
+			return;
+		}
 
-	@Subscribe
-	public void onUsernameChanged(UsernameChanged e) {
-	}
+		split = new ArrayList<>(Arrays.asList(search.toLowerCase().split("\\s+")));
 
-	@Subscribe
-	public void onVarClientIntChanged(VarClientIntChanged e) {
-	}
+		// find and record any slot filters and remove them from the search terms.
+		if (config.slotSearchEnabled())
+		{
+			for (int i = 0; i < split.size(); i++)
+			{
+				String s = split.get(i);
+				if (s.endsWith("slot"))
+				{
+					s = s.substring(0, s.length() - "slot".length());
+					System.out.println(s);
+				}
+				boolean found = true;
+				switch (s)
+				{
+					case "leg":
+					case "legs":
+						slotsBitfield |= 1 << EquipmentInventorySlot.LEGS.getSlotIdx();
+						break;
+					case "chest":
+					case "body":
+					case "torso":
+						slotsBitfield |= 1 << EquipmentInventorySlot.BODY.getSlotIdx();
+						break;
+					case "glove":
+					case "gloves":
+					case "hand":
+					case "hands":
+						slotsBitfield |= 1 << EquipmentInventorySlot.GLOVES.getSlotIdx();
+						break;
+					case "head":
+					case "helm":
+					case "hat":
+						slotsBitfield |= 1 << EquipmentInventorySlot.HEAD.getSlotIdx();
+						break;
+					case "boot":
+					case "boots":
+					case "foot":
+					case "feet":
+						slotsBitfield |= 1 << EquipmentInventorySlot.BOOTS.getSlotIdx();
+						break;
+					case "amulet":
+					case "necklace":
+					case "neck":
+						slotsBitfield |= 1 << EquipmentInventorySlot.AMULET.getSlotIdx();
+						break;
+					case "ring":
+						slotsBitfield |= 1 << EquipmentInventorySlot.RING.getSlotIdx();
+						break;
+					case "shield":
+					case "offhand":
+						slotsBitfield |= 1 << EquipmentInventorySlot.SHIELD.getSlotIdx();
+						break;
+					case "cape":
+					case "cloak":
+					case "back":
+						slotsBitfield |= 1 << EquipmentInventorySlot.CAPE.getSlotIdx();
+						break;
+					case "ammo":
+						slotsBitfield |= 1 << EquipmentInventorySlot.AMMO.getSlotIdx();
+						break;
+					case "weapon":
+						slotsBitfield |= 1 << EquipmentInventorySlot.WEAPON.getSlotIdx();
+						break;
+					case "1h":
+					case "mainhand":
+					case "1hweapon":
+					case "weaponslot1h":
+						slotsBitfield |= 1 << EquipmentInventorySlot.WEAPON.getSlotIdx();
+						twohand = Boolean.FALSE;
+						break;
+					case "2h":
+					case "2hweapon":
+					case "weaponslot2h":
+						slotsBitfield |= 1 << EquipmentInventorySlot.WEAPON.getSlotIdx();
+						twohand = Boolean.TRUE;
+						break;
+					default:
+						found = false;
+				}
+				if (found)
+				{
+					split.remove(i);
+					i--;
+				}
+			}
+		}
 
-	@Subscribe
-	public void onVarClientStrChanged(VarClientStrChanged e) {
-	}
-
-	@Subscribe
-	public void onVarbitChanged(VarbitChanged e) {
-	}
-
-	@Subscribe
-	public void onVolumeChanged(VolumeChanged e) {
-	}
-
-	@Subscribe
-	public void onWallObjectDespawned(WallObjectDespawned e) {
-	}
-
-	@Subscribe
-	public void onWallObjectSpawned(WallObjectSpawned e) {
-	}
-
-	@Subscribe
-	public void onWidgetClosed(WidgetClosed e) {
-	}
-
-	@Subscribe
-	public void onWidgetLoaded(WidgetLoaded e) {
-	}
-
-	@Subscribe
-	public void onWorldChanged(WorldChanged e) {
-	}
-
-	@Subscribe
-	public void onWorldListLoad(WorldListLoad e) {
+		// disable multisearch if multisearch is disabled
+		// do not disable multisearch if slot search is in use. This is convenient for queries that contain both a slot filter and other search text.
+		// disable multisearch when there are no search terms left. This is necessary for queries that only contain slot filters.
+		if (
+			!config.multiSearchEnabled() && slotsBitfield == 0 ||
+			split.size() == 0
+		) {
+			split = null;
+		}
+		System.out.println("slots bitfield " + slotsBitfield + " split " + split);
 	}
 }
